3e268721e9ca14ff3585dfc56a1e698791643c21
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-02 16:58:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    self.find_by_name(name) || self.create_by_name(name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
   def self.alphabetical
