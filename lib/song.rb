265bbbcc7c6375f0404f56901b3ab1417feffd81
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-02 17:14:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,17 +51,14 @@ class Song
         #"Taylor Swift"
     song_name_string = artist_name_array[1].delete_suffix(".mp3")
         #"Blank Space"
-
-    song = self.new
-    song.name = song_name_string
     song.artist_name = artist_string
-    song
+    self.new_by_name(song_name_string)
     #return new song instance with song name sent to title & artist set to artist
     #Taylor Swift - Blank Space.mp3
   end
 
   def self.create_from_filename(artist_and_name)
-  end 
+  end
 
 
 end
